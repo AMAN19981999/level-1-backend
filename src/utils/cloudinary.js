85b70fbcc735cbc63f339344278ff968631f75a5
AMAN19981999 <amanbhar0050@gmail.com>
2024-09-08 16:52:30 +0530

    old changes

--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -1,8 +1,41 @@
-import { v2 as clouddata } from "cloudinary";
+import { v2 as cloudinary } from "cloudinary";
 import fs from "fs";
 import "dotenv/config";
+
 cloudinary.config({
-  cloud_name: "dbbitiuj7",
-  api_key: "858649295211382",
-  api_secret: "7HL5", 
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_CLOUD_APIKEY,
+  api_secret: process.env.CLOUDINARY_CLOUD_API_SECRET,
 });
+
+const uploadoncloudinary = async (localfilepath) => {
+  try {
+    if (!localfilepath) return null;
+    //uplaod fike
+    const response = await cloudinary.uploader.upload(localfilepath, {
+      resource_type: "auto",
+    });
+    //uploadedfile
+    console.log("uplaoded the file", response.url);
+
+    return response;
+  } catch (error) {
+    fs.unlinkSync(localfilepath); // remove the locally saved path temproary if operation failed
+  }
+};
+
+export { uploadoncloudinary };
+
+// Upload an image
+// const uploadResult = await cloudinary.uploader
+//   .upload(
+//     "https://res.cloudinary.com/demo/image/upload/getting-started/shoes.jpg",
+//     {
+//       public_id: "shoes",
+//     }
+//   )
+//   .catch((error) => {
+//     console.log(error);
+//   });
+
+// console.log(uploadResult);
