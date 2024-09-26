248610a2f8d42060d71a120d8a6f8a22ee7ddcb9
AMAN19981999 <amanbhar0050@gmail.com>
2024-09-26 17:56:51 +0530

    aaaaa

---
+++ src/models/subscription.model.js
@@ -0,0 +1,22 @@
+import mongoose, { Schema } from "mongoose";
+import { User } from "./user.model";
+
+const subscriptonSchema = new Schema(
+  {
+    subscriber :{
+        type: Schema.Types.ObjectId,//one who is subscribing
+        ref: User
+    },
+    channel : {
+        type: Schema.Types.ObjectId,//one whoes channel is being subscribed
+        ref: User
+
+    }
+
+  },
+  {
+    timestamps: true,
+  }
+);
+
+export const Subscription = mongoose.model("Subscription", subscriptonSchema);
